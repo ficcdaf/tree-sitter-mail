d874779b4ddf829c8dca5446e18b89ee668ab073
Daniel Fichtinger <daniel@ficd.ca>
2025-02-17 20:05:46 -0500

    feat: better highlights

--- queries/mail/highlights.scm
+++ queries/mail/highlights.scm
@@ -1,4 +1,23 @@
-(header_email) @type.builtin
+; header fields
+[
+  (header_field_email)
+  (header_field_subject)
+  (header_field)
+] @keyword
+
+(header_separator) @punctuation.delimiter
+
+(header_subject
+  (header_unstructured) @markup.heading)
+(header_other
+  (header_unstructured) @comment)
+
+; Firstname Lastname
+(atom) @variable
+
+; Email Address
+; currently this includes the wrapping <>, can we change this?
 (email) @string
-(header_subject) @tag
-(header_other) @markup.environment
+
+; (header_subject) @markup.heading
+; (header_other) @markup.environment
