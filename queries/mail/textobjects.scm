e3263d70822add530fe35dd73187611f5403d271
Daniel Fichtinger <daniel@ficd.ca>
2025-02-23 01:33:19 -0500

    updated textobject queries

--- queries/mail/textobjects.scm
+++ queries/mail/textobjects.scm
@@ -1,2 +1,9 @@
 (atom_block
   (atom) @entry.inside) @entry.around
+(email_address) @entry.around
+; (header_subject
+;   (header_unstructured) @entry.around)
+(header_unstructured) @entry.around
+
+; (quote_contents) @comment.inside
+(quote_block)+ @comment.around
